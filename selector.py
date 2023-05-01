26e0b920b97e69346d7a0a312538cb62bdcb8c40
JoonhyeonKim <joonhyeonkim1015@gmail.com>
2023-05-01 14:30:39 +0900

    feat: Range change

--- selector.py
+++ selector.py
@@ -6,5 +6,5 @@ import random
 n = int(input('How many time do you wanna get random numbers?: '))
 
 for i in range(n):
-    l = random.sample(range(0, 100), 6)
+    l = random.sample(range(6, 45), 6)
     print(l)
